11b50f2c5a1022ddfe408e271e710fe5f91977c4
nanogarden-org <nanogarden@gmail.com>
2026-08-18 04:46:18 -0400

    Add vault package namespace

---
+++ src/phonic_drive/vault/__init__.py
@@ -0,0 +1,5 @@
+"""Session Vault support for Phonic Drive."""
+
+from .identity import artifact_identity, new_managed_id, sha256_file
+
+__all__ = ["artifact_identity", "new_managed_id", "sha256_file"]
