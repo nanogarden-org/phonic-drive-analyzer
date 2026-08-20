0cdf32da17385dc748d4bc819b619ed3c3869d12
nanogarden-org <nanogarden@gmail.com>
2026-08-19 20:20:22 -0400

    expose trial manifest and randomization primitives

--- phonic_drive/trials/__init__.py
+++ phonic_drive/trials/__init__.py
@@ -1,4 +1,8 @@
 """User-trial primitives for Phonic Drive v3."""
 from .events import EventRecorder, ResponseEvent, event_windows, nearest_motifs
+from .protocol import StimulusCondition, TrialManifest, build_manifest, randomized_order, stable_seed
 
-__all__ = ["EventRecorder", "ResponseEvent", "event_windows", "nearest_motifs"]
+__all__ = [
+    "EventRecorder", "ResponseEvent", "event_windows", "nearest_motifs",
+    "StimulusCondition", "TrialManifest", "build_manifest", "randomized_order", "stable_seed",
+]
