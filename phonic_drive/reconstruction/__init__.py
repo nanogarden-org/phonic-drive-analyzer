091ea09b205dcb9e67dfda2231cad0b56c91e283
nanogarden-org <nanogarden@gmail.com>
2026-08-19 20:20:38 -0400

    expose reconstruction provenance primitives

---
+++ phonic_drive/reconstruction/__init__.py
@@ -0,0 +1,4 @@
+"""Reconstruction and ablation provenance for Phonic Drive v3."""
+from .protocol import ReconstructionManifest, TransformStep, ablation_manifest
+
+__all__ = ["ReconstructionManifest", "TransformStep", "ablation_manifest"]
