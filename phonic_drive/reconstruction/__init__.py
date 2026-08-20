3be0fd59420a4a814d071571936a3bd20b9befc7
nanogarden-org <nanogarden@gmail.com>
2026-08-19 20:27:03 -0400

    Expose reconstruction transforms

--- phonic_drive/reconstruction/__init__.py
+++ phonic_drive/reconstruction/__init__.py
@@ -1,4 +1,24 @@
-"""Reconstruction and ablation provenance for Phonic Drive v3."""
+"""Reconstruction, ablation, and provenance for Phonic Drive v3."""
 from .protocol import ReconstructionManifest, TransformStep, ablation_manifest
+from .transforms import (
+    circular_shift,
+    envelope_noise_control,
+    reverse_time,
+    rms,
+    rms_match,
+    segment_shuffle,
+    spectral_ablation,
+)
 
-__all__ = ["ReconstructionManifest", "TransformStep", "ablation_manifest"]
+__all__ = [
+    "ReconstructionManifest",
+    "TransformStep",
+    "ablation_manifest",
+    "rms",
+    "rms_match",
+    "reverse_time",
+    "circular_shift",
+    "spectral_ablation",
+    "segment_shuffle",
+    "envelope_noise_control",
+]
