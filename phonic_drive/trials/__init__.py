a745420c54faf4573d8f6b27fd41a532edeff703
nanogarden-org <nanogarden@gmail.com>
2026-08-19 20:18:43 -0400

    expose trial event primitives

---
+++ phonic_drive/trials/__init__.py
@@ -0,0 +1,4 @@
+"""User-trial primitives for Phonic Drive v3."""
+from .events import EventRecorder, ResponseEvent, event_windows, nearest_motifs
+
+__all__ = ["EventRecorder", "ResponseEvent", "event_windows", "nearest_motifs"]
